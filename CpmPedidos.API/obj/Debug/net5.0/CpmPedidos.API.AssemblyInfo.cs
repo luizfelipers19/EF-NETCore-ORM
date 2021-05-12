305a73533277eb65e0b6f431268ea2fef700b537
LuizFelipe <luizfelipers19@gmail.com>
2021-05-11 22:03:51 -0300

    created a new ASP.NET Core WebAPI project inside the main project

--- CpmPedidos.API/obj/Debug/net5.0/CpmPedidos.API.AssemblyInfo.cs
+++ CpmPedidos.API/obj/Debug/net5.0/CpmPedidos.API.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CpmPedidos.interface")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CpmPedidos.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("CpmPedidos.interface")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CpmPedidos.interface")]
+[assembly: System.Reflection.AssemblyProductAttribute("CpmPedidos.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CpmPedidos.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
