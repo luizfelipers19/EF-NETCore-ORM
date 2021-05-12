05d3a45e46ea90672de841a92763dd08393f0a81
LuizFelipe <luizfelipers19@gmail.com>
2021-05-11 22:14:59 -0300

    cleaned the webAPI project by renaming the standard weatherforecastController and deleting it's methods, deleted the weatherForecast class and added references from all other projects to the webAPI proj

---
+++ CpmPedidos.API/Controllers/PedidoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpmPedidos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {
+
+        public PedidoController()
+        {
+            
+        }
+
+       
+    }
+}
