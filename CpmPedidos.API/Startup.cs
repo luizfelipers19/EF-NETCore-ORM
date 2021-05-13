f7027f2a87559f4ce9ee608ac65f58ea4635795c
LuizFelipe <luizfelipers19@gmail.com>
2021-05-12 22:10:14 -0300

    added DbContext support

--- CpmPedidos.API/Startup.cs
+++ CpmPedidos.API/Startup.cs
@@ -10,21 +10,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.Common;
+using Npgsql;
+using CpmPedidos.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace CpmPedidos.API
 {
     public class Startup
     {
+        
+        public IConfiguration Configuration { get; }
+
+        public DbConnection dbConnection => new NpgsqlConnection(Configuration.GetConnectionString("App"));
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(
+                    DbConnection,
+                    assembly => assembly.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+
 
             DependencyInjection.Register(services);
 
---
+++ CpmPedidos.Repository/Common/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Repository
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        protected override OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+        }
+
+        public ApplicationDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+        
+    }
+}
