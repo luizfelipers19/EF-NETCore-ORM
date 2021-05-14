2bd965cbde0d91b2a6e28c2dd94218f80009d4f2
LuizFelipe <luizfelipers19@gmail.com>
2021-05-13 22:28:31 -0300

    created the first domain classes, in a way that an abstract base-class will be used as parent class to other classes to inherit from, and also implemented an interface to hold trivial properties

---
+++ CpmPedidos.Domain/Entities/Cidade.cs
@@ -0,0 +1,21 @@
+﻿using CpmPedidos.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Domain.Entities
+{
+    public class Cidade : BaseDomain, IExibivel
+    {
+      
+
+        public string Nome { get; set; }
+
+        public string UF { get; set; }
+
+        public bool Ativado { get; set; }
+
+    }
+}
